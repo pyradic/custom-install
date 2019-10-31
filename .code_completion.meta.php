6158bbcf22ed4f8b9f8e794796e8c0d349529492
Robin Radic <rradic@hotmail.com>
2019-10-31 06:15:15 +0100

    switch to build tools

--- .code_completion.meta.php
+++ .code_completion.meta.php
@@ -3,15 +3,10 @@
 
 
 namespace Illuminate\Support {
-
-    /**
-     *
-     *
-     * @mixin \Illuminate\Support\Collection
-     */
-    class Collection {
-        public function cut(array $names) {
-            return \Illuminate\Support\Collection::cut($names);
-        }
-    }
+//
+//    class Collection {
+//        public function cut(array $names) {
+//            return \Illuminate\Support\Collection::cut($names);
+//        }
+//    }
 }
