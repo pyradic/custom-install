68dc1ad1186f3c4108042e39c743a1d4c288bb14
Robin Radic <rradic@hotmail.com>
2019-12-21 01:55:43 +0100

    upd

--- src/Console/UninstallCommand.php
+++ src/Console/UninstallCommand.php
@@ -2,6 +2,8 @@
 
 namespace Pyro\CustomInstall\Console;
 
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Schema\View;
 use Illuminate\Console\Command;
 use Illuminate\Console\ConfirmableTrait;
 
@@ -13,26 +15,56 @@ class UninstallCommand extends Command
 
     protected $description = 'Uninstall the application';
 
+    /**
+     * @var \Illuminate\Database\DatabaseManager
+     */
+    protected $db;
+
+    /** @var \Doctrine\DBAL\Schema\AbstractSchemaManager */
+    protected $schema;
+
     public function handle()
     {
         if ( ! $this->confirmToProceed()) {
             return;
         }
+        $this->db     = $this->getLaravel()->make('db');
+        $this->schema = $schema= $this->db->getDoctrineSchemaManager();
+
         $this->info('Setting INSTALLED to false');
         $this->call('env:set', [ 'line' => 'INSTALLED=false' ]);
 
-
-        $db     = $this->getLaravel()->make('db');
-        $schema = $db->getDoctrineSchemaManager();
         foreach ($schema->listViews() as $view) {
-            $this->line('Dropping view: ' . $view->getName());
-            $schema->dropView($view->getName());
+            $this->dropView($view);
         }
+
+        /** @var Table[] $fktables */
+        $fktables = array_filter($schema->listTables(),function(Table $table){
+            return count($table->getForeignKeys()) > 0;
+        });
+
+        foreach($fktables as $table){
+            $this->dropTable($table);
+        }
+
         foreach ($schema->listTables() as $table) {
-            $this->line('Dropping table: ' . $table->getName());
-            $schema->dropTable($table->getName());
+            $this->dropTable($table);
         }
+
         $this->info('Truncated database');
         $this->info('Application uninstalled');
+    }
+
+    protected function dropView(View $view)
+    {
+        $this->line('Dropping view: ' . $view->getName());
+        $this->schema->dropView($view->getName());
+
+    }
+
+    protected function dropTable(Table $table)
+    {
+        $this->line('Dropping table: ' . $table->getName());
+        $this->schema->dropTable($table->getName());
     }
 }
